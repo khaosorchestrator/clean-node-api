9224c6d5d74e0b6da6d0744821990e912bc0a87a
Lucas Almeida <developer.lucas24@gmail.com>
2021-04-12 22:40:02 +0100

    refactor: transform handle to async

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
-import { ServerError } from '../errors/server-error'
-import { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
