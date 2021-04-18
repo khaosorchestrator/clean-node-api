6b04a952af00470c9d94163b2ca3f6a0839d0f9d
Lucas Almeida <developer.lucas24@gmail.com>
2021-04-18 12:57:18 +0100

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir/src/**/*.ts>',
     '!<rootDir/src/main/**>',
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,14 +1,18 @@
+import { AddAccountRepository } from '../../protocols/add-account-repository'
 import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) {
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+    const hashedPassword = await this.encrypter.encrypt(account.password)
+    await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }))
     return new Promise(resolve => resolve({
       id: 'valid_id',
       name: 'valid_name',
