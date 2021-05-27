cdaa01284243d27c7e2cd4cef0a2d4911f94f9f9
Lucas Almeida <developer.lucas24@gmail.com>
2021-05-27 23:44:44 +0100

    fix: create code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir/src/**/*.ts>',
-    '!<rootDir/src/main/**>',
-    '!<rootDir/src/**/*-protocols.ts>',
-    '!**/protocols/**>',
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
     '!**/test/**'
   ],
   coverageDirectory: 'coverage',
